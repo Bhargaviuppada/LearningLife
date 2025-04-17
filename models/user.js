a58fd8856b87ec47a2c84f5baaf89349a8f09608
Bhargaviuppada <165626603+Bhargaviuppada@users.noreply.github.com>
2025-04-17 17:17:08 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,9 +17,27 @@ const userSchema = new mongoose.Schema({
   },
   enrolledCourses: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  }]
+    ref: 'Course',
+  }],
 });
+
+// Hash the password before saving the user
+userSchema.pre('save', function(next) {
+  if (this.isModified('password')) {  // Hash password only if it's new or modified
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) return next(err);
+      this.password = hashedPassword; // Replace the plain password with the hashed one
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
+// Compare the provided password with the stored hashed password
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
