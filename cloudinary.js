a9192f9aa5a7069d8f7e7b16fb88f6b5e3e705db
Bhargaviuppada <165626603+Bhargaviuppada@users.noreply.github.com>
2025-04-17 21:51:17 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,19 +1,22 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary: cloudinary, // Make sure it's v2
   params: {
-    folder: 'LearningLifeUploads',
-    allowed_formats: ['jpg', 'png', 'mp4']
-  }
+    folder: "LearningLifeCourses",
+    allowed_formats: ["jpg", "png", "jpeg", "mp4"],
+  },
 });
 
-module.exports = { cloudinary, storage };
+module.exports = {
+  cloudinary,
+  storage,
+};
 
