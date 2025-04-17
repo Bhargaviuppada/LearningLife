a7825ada7e8647662bf5ee3f3307f64ab920b2bb
Bhargaviuppada <165626603+Bhargaviuppada@users.noreply.github.com>
2025-04-17 17:17:55 +0530

    Update course.js

--- models/course.js
+++ models/course.js
@@ -3,21 +3,22 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   timeRequired: {
     type: Number,
-    required: true
+    required: true,
   },
   image: {
     type: String,
-    required: true
+    required: true,
   },
   videos: {
     type: [String],  // Array of strings (video names or links)
-    required: true
-  }
+    required: true,
+  },
 });
 
 const Course = mongoose.model('Course', courseSchema);
 module.exports = Course;
+
