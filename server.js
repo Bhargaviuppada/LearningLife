d8b279a033ba1a2abb6716552e458750d841aec6
Bhargaviuppada <165626603+Bhargaviuppada@users.noreply.github.com>
2025-05-02 07:45:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 // Setup
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
